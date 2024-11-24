7ef7c4da40b2639d49f38a8c42713b9ff2ea87e4
Barmington Chuku <barmingtonk@gmail.com>
2024-11-24 13:45:04 +0000

    configured

--- src/app/blogs/newBlogs/page.js
+++ src/app/blogs/newBlogs/page.js
@@ -1,9 +1,9 @@
-import NewBlog from '../../Components/NewBlog';
+import Newblog from '@/Components/Newblog.jsx';
 export default function AddBlog() {
   return (
     <div className="m-20">
       <h2>Add New Post</h2>
-      <NewBlog />
+      <Newblog />
     </div>
   );
 }
